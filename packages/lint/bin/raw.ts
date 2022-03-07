5bf52c8b075e0746209a6a1fbdbcc43b9444d615
chenyueban <jasonchan0527@gmail.com>
2022-03-07 17:31:14 +0800

    feat(lint): 修改 lint-staged 配置

--- packages/lint/bin/raw.ts
+++ packages/lint/bin/raw.ts
@@ -35,8 +35,8 @@ export const PRETTIERRC = `{
 `
 
 export const LINTSTAGEDRC = `{
-  "*.{less,sass,scss,md,json,yml}": ["prettier --write", "git add"],
-  "*.{js,jsx,html}": ["prettier --write", "eslint --fix", "git add"],
+  "*.{md,json,yml}": ["prettier --write", "git add"],
+  "*.{js,jsx,vue,html}": ["prettier --write", "eslint --fix", "git add"],
   "*.ts?(x)": [
     "prettier --parser=typescript --write",
     "eslint --fix",
