a9bf5440089f0dccc3d855785236f6f4f195d280
chenyueban <jasonchan0527@gmail.com>
2022-06-28 10:59:49 +0800

    feat(eslint-config): remove curly rule

--- packages/eslint-config/src/basic.js
+++ packages/eslint-config/src/basic.js
@@ -137,7 +137,6 @@ module.exports = {
 
     // Common
     'semi': ['error', 'never'],
-    'curly': ['error', 'multi-or-nest', 'consistent'],
     'quotes': ['error', 'single'],
     'quote-props': ['error', 'consistent-as-needed'],
     'no-unused-vars': 'warn',
