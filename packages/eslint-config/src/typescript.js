7b5e85177186fe02f44af294e6dd0f7c546cb00a
chenyueban <jasonchan0527@gmail.com>
2023-09-05 14:47:57 +0800

    chore(eslint-config): update @typescript-eslint/consistent-type-imports configuration

--- packages/eslint-config/src/typescript.js
+++ packages/eslint-config/src/typescript.js
@@ -23,7 +23,7 @@ export const typescript = [
       '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
       '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
       '@typescript-eslint/type-annotation-spacing': ['error', {}],
-      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', fixStyle: 'separate-type-imports', disallowTypeAnnotations: false }],
       '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
       '@typescript-eslint/prefer-ts-expect-error': 'error',
       '@typescript-eslint/no-require-imports': 'error',
